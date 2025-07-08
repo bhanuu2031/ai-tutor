2027439f3006719b884f16257f0d249d7741e5e3
bhanuu2031 <bhannuu2031@gmail.com>
2025-07-08 17:45:11 +0530

    Add files via upload

---
+++ TopicFilter.js
@@ -0,0 +1,36 @@
+// src/components/TopicFilter.js
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function TopicFilter({ onSelectTopic }) {
+  const [topics, setTopics] = useState([]);
+
+
+  
+  useEffect(() => {
+    axios.get("http://192.168.29.163:5000/topics")
+      .then(res => {
+        console.log("TOPICS RECEIVED:", res.data);  // ✅ Check console
+        setTopics(res.data);
+      })
+      .catch(err => {
+        console.error("Error fetching topics:", err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <label>
+        <strong>Select a Topic: </strong>
+        <select onChange={(e) => onSelectTopic(e.target.value)}>
+          <option value="">--Choose Topic--</option>
+          {topics.map((topic, idx) => (
+            <option key={idx} value={topic}>{topic}</option>
+          ))}
+        </select>
+      </label>
+    </div>
+  );
+}
+
+export default TopicFilter;
